0e880d04d46b5ef7705308561a8db2d9247e5fdf
NikhilKumarReddyKadiveti <sowjanyakadiveti1037@gmail.com>
2026-05-26 22:45:31 +0530

    Fix Supabase proxy cookie types

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,4 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function proxy(request: NextRequest) {
@@ -44,43 +44,6 @@ export async function proxy(request: NextRequest) {
               value,
               ...options,
             })
-          })
-        },
-        get(name: string) {
-          return request.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
           })
         },
       },
